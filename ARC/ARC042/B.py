eb068e6334d85b3a8d280a83f020fb90e7322a1d
ryusuke920 <ryusuke920@AtCoder-Library.com>
2021-03-08 13:47:58 +0900

    syouzin

--- ARC/ARC042/B.py
+++ ARC/ARC042/B.py
@@ -1,10 +1,18 @@
 import math
-x,y = map(int,input().split())
+x, y = map(int,input().split())
 n = int(input())
-ans = 10**9
+ans = 10 ** 9
+x = [0] * n
+y = [0] * n
 for i in range(n):
-    a,b = map(int,input().split())
-    x1 = (x-a)**2
-    y1 = (y-b)**2
-    ans = min(ans, math.sqrt(x1+y1))
+    x[i], y[i] = map(int,input().split())
+
+for i in range(n - 1):
+    if x[i] - x[i + 1] == 0:
+        d = abs(x - x[i])
+    else:
+        a = (y[i] - y[i + 1]) / (x[i] - x[i + 1])
+        b = (x[i] * y[i + 1] - x[i + 1] * y[i]) / (x[i] - x[i + 1])
+        d = (abs(a * x[i] - y[i] + b)) / (math.sqrt(a ** 2 + 1))
+    ans = min(d, ans)
 print(ans)
